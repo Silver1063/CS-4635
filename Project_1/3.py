44866aec271790784b3d2ba80489ecb6ed06e21a
Silver1063 <brickbrickerson1063@gmail.com>
2024-02-14 13:43:54 -0800

    Changes to files and generated histogram

--- Project_1/3.py
+++ Project_1/3.py
@@ -1,17 +1,16 @@
 # Group Members
 # ,, Edward Eckle,
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-# parameters
-size = 10_000_000
+nums = []
 num_intervals = 10
 
-nums = []
-random.seed(1)
-for i in range(size):
-    nums.append(random.random())
+fname: str = "Project_1/random.out"
+
+with open(fname, "r") as file:
+    for num in file.readlines():
+        nums.append(eval(num))
 
 counts, bins = np.histogram(nums, num_intervals)
 
@@ -24,20 +23,20 @@ plt.xticks(np.arange(0.0, 1.1, 0.1))
 plt.show()
 
 # X^2 test parameters
-X2 = 0
-Oi = 0
-Ei = size / num_intervals
+# X2 = 0
+# Oi = 0
+# Ei = size / num_intervals
 
-# Iterate through each interval
-for i in range(num_intervals):
-    interval = (i / num_intervals, (i + 1) / num_intervals)
-    Oi = 0
-    num_i = []
-    for num in nums:
-        if num >= interval[0] and num < interval[1]:
-            num_i.append(num)
-            Oi += 1
-    X2 += (Oi - Ei) ** 2 / Ei
+# # Iterate through each interval
+# for i in range(num_intervals):
+#     interval = (i / num_intervals, (i + 1) / num_intervals)
+#     Oi = 0
+#     num_i = []
+#     for num in nums:
+#         if num >= interval[0] and num < interval[1]:
+#             num_i.append(num)
+#             Oi += 1
+#     X2 += (Oi - Ei) ** 2 / Ei
 
 
-print("X^2 value:", X2)
+# print("X^2 value:", X2)
